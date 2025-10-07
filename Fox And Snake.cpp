a9c3edd8d3f73996ded71b795c4a4dce83ef52e7
Abu Bakar Siddique <111673862+siddique0203@users.noreply.github.com>
2025-10-07 20:57:32 +0600

    Add files via upload

---
+++ Fox And Snake.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    bool flag=true;
+
+    for(int i=0; i<n; i++){
+        if(i%2==0){
+             for(int j=0; j<m; j++){
+                cout<<"#";
+             }  cout<<endl;
+        }
+        else{
+            if(flag){
+                for(int j=0; j<m-1; j++){
+                    cout<<".";
+                }cout<<"#"<<endl;
+                flag=!flag;
+            }
+            else{
+                cout<<"#";
+                for(int j=0; j<m-1; j++){
+                    cout<<".";
+                }
+            cout<<endl;
+            flag=!flag;
+            }
+        }
+    }
+}
